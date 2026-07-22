e3df530ee3de0021947227629f39c2d15e057f2f
Almo-ond-tech <amadankwah2008@gmail.com>
2026-07-22 16:16:02 +0200

    shop sprite animation

--- script.js
+++ script.js
@@ -16,6 +16,17 @@ const background = new Sprite({
     imageSrc: './assets/background3.jpg'
 })
 
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 120
+    },
+    imageSrc: './assets/shop.png',
+    scale: 2.5,
+    framesMax: 6 
+
+})
+
 const player = new Fighter({
     
     position: {
@@ -82,6 +93,7 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
 
     background.update()
+    shop.update()
 
     player.update()
     enemy.update()
